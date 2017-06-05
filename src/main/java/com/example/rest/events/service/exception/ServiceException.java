d5beee4329ff3821a6418cc1101462000ace4f4a
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-06-06 00:10:43 +0300

    Some changes

--- src/main/java/com/example/rest/events/service/exception/ServiceException.java
+++ src/main/java/com/example/rest/events/service/exception/ServiceException.java
@@ -9,11 +9,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ServiceException extends RuntimeException {
 
-    public ServiceException(String message) {
-        super(message);
-    }
-
-    public ServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
